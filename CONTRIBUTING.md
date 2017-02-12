9aa57a97a13b4c7ff48d86cedc2b3b32c8916ada
Andrej Burenins <burenins@gmail.com>
2017-02-12 08:48:12 +0000

    My 1-st commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,2 @@
+Contributions
+contr1# test line
---
+++ src/my/git/testing/SomeClass.java
@@ -0,0 +1,10 @@
+package my.git.testing;
+
+public class SomeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
